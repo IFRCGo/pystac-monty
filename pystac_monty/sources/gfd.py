8577dc29dbda5c54d534a02d7f3d10c9ff3b7c6e
sandeshit <thapasandesh56@gmail.com>
2025-03-19 16:06:46 +0545

    GFD data validators

--- pystac_monty/sources/gfd.py
+++ pystac_monty/sources/gfd.py
@@ -15,7 +15,7 @@ from pystac_monty.extension import (
 )
 from pystac_monty.hazard_profiles import MontyHazardProfiles
 from pystac_monty.sources.common import MontyDataSource, MontyDataTransformer
-
+from pystac_monty.validators.gfd import GFDSourceValidator
 # Constants
 
 STAC_EVENT_ID_PREFIX = "gfd-event-"
@@ -28,8 +28,21 @@ class GFDDataSource(MontyDataSource):
 
     def __init__(self, source_url: str, data: Any):
         super().__init__(source_url, data)
-        self.data = json.loads(data)
+        self.data = self.source_data_validator(json.loads(data))
 
+    def source_data_validator(self, data: list[dict]):
+        """Validate the source data and collect only the success items"""
+        # TODO Handle the failed_items
+        failed_items = []
+        success_items = []
+        for item in data:
+            is_valid = GFDSourceValidator.validate_event(item)    
+            if is_valid:
+                success_items.append(item)
+            else:
+                failed_items.append(item)
+        return success_items
+        
 
 class GFDTransformer(MontyDataTransformer):
     """Transform the source data into the STAC items"""
