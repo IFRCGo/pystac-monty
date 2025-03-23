fd12c229286e7666ae2b574a37abb1f26e0f50cc
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-03-23 18:12:34 +0545

    Apply the partial success + generator while building the stac items

--- pystac_monty/sources/idu.py
+++ pystac_monty/sources/idu.py
@@ -1,9 +1,11 @@
 import datetime
 import json
 import logging
+import itertools
 import re
 from dataclasses import dataclass
 from typing import Any, Dict, List
+import typing
 
 import pytz
 from markdownify import markdownify as md
@@ -35,20 +37,7 @@ class IDUDataSource(MontyDataSource):
 
     def __init__(self, source_url: str, data: Any):
         super().__init__(source_url, data)
-        self.data = self.source_data_validator(json.loads(data))
-
-    def source_data_validator(self, data: list[dict]):
-        """Validate the source data and collect only the success items"""
-        # TODO Handle the failed_items
-        failed_items = []
-        success_items = []
-        for item in data:
-            is_valid = IDUSourceValidator.validate_event(item)
-            if is_valid:
-                success_items.append(item)
-            else:
-                failed_items.append(item)
-        return success_items
+        self.data = json.loads(data)
 
 
 class IDUTransformer(MontyDataTransformer[IDUDataSource]):
@@ -57,81 +46,92 @@ class IDUTransformer(MontyDataTransformer[IDUDataSource]):
     hazard_profiles = MontyHazardProfiles()
     source_name = 'idmc-idu'
 
-    def make_items(self) -> List[Item]:
-        """Create items"""
-        items = []
+    def get_stac_items(self) -> typing.Generator[Item, None, None]:
+        """Creates the STAC Items"""
+        failed_items_count = 0
+        total_items_count = 0
 
-        event_items = self.make_source_event_items()
-        # Get the latest item based on id(the last occurrence)
-        # and get rid of duplicate items at event level
-        event_items_unique = {item.id: item for item in event_items}
-        event_items = list(event_items_unique.values())
-        items.extend(event_items)
-
-        impact_items = self.make_impact_items()
-        items.extend(impact_items)
-
-        return items
-
-    def make_source_event_items(self) -> List[Item]:
-        """Create the source event item"""
-        items = []
         idu_data = self.check_and_get_idu_data()
-        if not idu_data:
-            return []
+        idu_data.sort(key=lambda x: x.get("event_id", " "))
+        total_items_count = len(idu_data)
+        for event_id, data_iterator in itertools.groupby(idu_data, key=lambda x: x.get("event_id", " ")):
+            idu_data_items = list(data_iterator)
 
-        for data in idu_data:
-            item = self.make_source_event_item(data=data)
-            items.append(item)
-        return items
+            idu_data_items.sort(key=lambda x: x.get("id"))
+            try:
+                def get_validated_data(items: list[dict]) -> List[IDUSourceValidator]:
+                    validated_data: list[IDUSourceValidator] = []
+                    for item in items:
+                        obj = IDUSourceValidator(**item)
+                        validated_data.append(obj)
+                    return validated_data
+                validated_data = get_validated_data(idu_data_items)
+                if event_item := self.make_source_event_item(event_id, validated_data):
+                    yield event_item
+                    yield from self.make_impact_item(event_item, validated_data)
+            except Exception:
+                failed_items_count += 1
+                logger.error("Failed to process the IDU data", exc_info=True)
 
-    def make_source_event_item(self, data: dict) -> Item:
+        logger.info(total_items_count)
+        logger.info(failed_items_count)
+
+    def make_items(self) -> List[Item]:
+        return list(self.get_stac_items())
+
+    def make_source_event_item(self, event_id: int, data_items: List[IDUSourceValidator]) -> Item:
         """Create an Event Item"""
-        latitude = float(data.get("latitude") or 0)
-        longitude = float(data.get("longitude") or 0)
+        # For now, get the first item only to create a single event item
+        data_item = data_items[0]
+        latitude = float(data_item.latitude or 0.0)
+        longitude = float(data_item.longitude or 0.0)
         # Create the geojson point
         point = Point(longitude, latitude)
         geometry = mapping(point)
         bbox = [longitude, latitude, longitude, latitude]
 
-        description = md(data["standard_popup_text"])
+        description = md(data_item.standard_popup_text)
 
         # Episode number not in the source, so, set it to 1
         episode_number = 1
 
-        startdate_str = data["event_start_date"]
-        enddate_str = data["event_end_date"]
+        startdate_str = data_item.event_start_date.strftime("%Y-%m-%d")
+        enddate_str = data_item.event_end_date.strftime("%Y-%m-%d")
 
-        startdate = pytz.utc.localize(datetime.datetime.fromisoformat(startdate_str))
-        enddate = pytz.utc.localize(datetime.datetime.fromisoformat(enddate_str))
+        startdate = pytz.utc.localize(
+            datetime.datetime.fromisoformat(startdate_str)
+        )
+        enddate = pytz.utc.localize(
+            datetime.datetime.fromisoformat(enddate_str)
+        )
 
         item = Item(
-            id=f"{STAC_EVENT_ID_PREFIX}{data['event_id']}",
+            id=f"{STAC_EVENT_ID_PREFIX}{data_item.event_id}",
             geometry=geometry,
             bbox=bbox,
             datetime=startdate,
             properties={
-                "title": data["event_name"],
+                "title": data_item.event_name,
                 "description": description,
                 "start_datetime": startdate.isoformat(),
                 "end_datetime": enddate.isoformat(),
-                "location": data["locations_name"],
-                "sources": data["sources"],
+                "location": data_item.locations_name,
+                "sources": data_item.sources,
             },
         )
 
         item.set_collection(self.get_event_collection())
         item.properties["roles"] = ["source", "event"]
 
-        item.add_asset("report", Asset(href=data["source_url"], media_type="application/pdf", title="Report"))
+        item.add_asset("report", Asset(href=data_item.source_url, media_type="application/pdf", title="Report"))
         item.add_link(Link("via", self.data_source.get_source_url(), "application/json", "IDU Event Data"))
 
-        hazard_tuple = (data["category"], data["subcategory"], data["type"], data["subtype"])
+        hazard_tuple = (data_item.category, data_item.subcategory, data_item.type, data_item.subtype)
 
         MontyExtension.add_to(item)
         monty = MontyExtension.ext(item)
         monty.episode_number = episode_number
-        monty.country_codes = [data["iso3"]]
+        monty.country_codes = [data_item.iso3]
         monty.hazard_codes = IDMCUtils.hazard_codes_mapping(hazard=hazard_tuple)
         monty.compute_and_set_correlation_id(hazard_profiles=self.hazard_profiles)
 
@@ -148,24 +148,25 @@ class IDUTransformer(MontyDataTransformer[IDUDataSource]):
         logger.warning(f"Match {match} not found. Using the default value.")
         return "displaced"
 
-    def make_impact_items(self) -> List[Item]:
+    def make_impact_item(self, event_item: Item, data_items: List[IDUSourceValidator]) -> List[Item]:
         """Create impact items"""
         items = []
-        idu_data = self.check_and_get_idu_data()
-        event_items = self.make_source_event_items()
-        for event_item, src_data in zip(event_items, idu_data):
+        #idu_data = self.check_and_get_idu_data()
+        #event_items = self.make_source_event_items()
+        # for event_item, src_data in zip(event_items, idu_data):
+        for data_item in data_items:
             impact_item = event_item.clone()
 
-            startdate_str = src_data["displacement_start_date"]
-            enddate_str = src_data["displacement_end_date"]
+            startdate_str = data_item.displacement_start_date.strftime("%Y-%m-%d")   # src_data["displacement_start_date"]
+            enddate_str = data_item.displacement_end_date.strftime("%Y-%m-%d")  # src_data["displacement_end_date"]
             startdate = pytz.utc.localize(datetime.datetime.fromisoformat(startdate_str))
             enddate = pytz.utc.localize(datetime.datetime.fromisoformat(enddate_str))
 
-            description = src_data["standard_popup_text"]
+            description = data_item.standard_popup_text  # src_data["standard_popup_text"]
             impact_type = self._get_impact_type_from_desc(description=description)
 
             impact_item.id = (
-                f"{impact_item.id.replace(STAC_EVENT_ID_PREFIX, STAC_IMPACT_ID_PREFIX)}{src_data['id']}-{impact_type}"
+                f"{impact_item.id.replace(STAC_EVENT_ID_PREFIX, STAC_IMPACT_ID_PREFIX)}{data_item.id}-{impact_type}"
             )
             impact_item.startdate = startdate
             impact_item.properties["start_datetime"] = startdate.isoformat()
@@ -174,12 +175,12 @@ class IDUTransformer(MontyDataTransformer[IDUDataSource]):
             impact_item.set_collection(self.get_impact_collection())
 
             monty = MontyExtension.ext(impact_item)
-            monty.impact_detail = self.get_impact_details(idu_src_item=src_data, impact_type=impact_type)
+            monty.impact_detail = self.get_impact_details(idu_src_item=data_item, impact_type=impact_type)
 
             items.append(impact_item)
         return items
 
-    def get_impact_details(self, idu_src_item: dict, impact_type: str):
+    def get_impact_details(self, idu_src_item: IDUSourceValidator, impact_type: str):
         """Returns the impact details related to displacement"""
         category, category_type = IDMCUtils.mappings.get(
             impact_type, (MontyImpactExposureCategory.ALL_PEOPLE, MontyImpactType.INTERNALLY_DISPLACED_PERSONS)
@@ -187,7 +188,7 @@ class IDUTransformer(MontyDataTransformer[IDUDataSource]):
         return ImpactDetail(
             category=category,
             type=category_type,
-            value=idu_src_item["figure"],
+            value=idu_src_item.figure,
             unit="count",
             estimate_type=MontyEstimateType.PRIMARY,
         )
