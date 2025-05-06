4f6302f5aa2c4d98306be93f6f7c3d40064579b4
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2025-05-06 16:45:31 +0545

    Fix the validation check for emdat source (admin_units)

--- pystac_monty/validators/em_dat.py
+++ pystac_monty/validators/em_dat.py
@@ -1,46 +1,37 @@
 import logging
-import math
-import typing
-from typing import Optional
-
-from pydantic import BaseModel, ConfigDict, field_validator
+from typing import Optional, Union, Any
+from pydantic import BaseModel, ConfigDict, field_validator, ValidationInfo
 import pandas as pd
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 logger.setLevel(logging.INFO)
 
-
 class BaseModelWithExtra(BaseModel):
     model_config = ConfigDict(extra="ignore")
 
 
+class Admin1(BaseModelWithExtra):
+    adm1_code: int
+    adm1_name: str
+
+class Admin2(BaseModelWithExtra):
+    adm2_code: int
+    adm2_name: str
+
 class EmdatDataValidator(BaseModelWithExtra):
     disno: str
     classif_key: str
-    # group: str
-    # subgroup: str
     type: str | None = None
     subtype: str | None = None
-    # external_ids: Optional[str] = None
     name: str | None = None
     iso: str | None
     country: str | None = None
-    # subregion: str
-    # region: str
     location: str | None = None
-    # origin: Optional[str] = None
-    # associated_types: Optional[str] = None
-    # ofda_response: bool
-    # appeal: bool
-    # declaration: bool
-    # aid_contribution: Optional[str] = None
     magnitude: float | None = None
     magnitude_scale: str | None = None
     latitude: float | None = None
     longitude: float | None = None
-    # river_basin: Optional[str] = None
-
     start_year: int | None
     start_month: int | None
     start_day: int | None
@@ -53,16 +44,8 @@ class EmdatDataValidator(BaseModelWithExtra):
     no_affected: float | None = None
     no_homeless: float | None = None
     total_affected: float | None = None
-    # reconstr_dam: Optional[float] = None
-    # reconstr_dam_adj: Optional[float] = None
-    # insur_dam: Optional[float] = None
-    # insur_dam_adj: Optional[float] = None
     total_dam: float | None = None
-    # total_dam_adj: Optional[float] = None
-    # cpi: Optional[float] = None
-    admin_units: list[typing.Any] | None = None
-    # entry_date: Optional[str]
-    # last_update: Optional[str]
+    admin_units: Optional[list[Union[Admin1, Admin2]]]
 
     @field_validator("total_deaths")
     def validate_total_deaths(cls, value):
@@ -70,22 +53,11 @@ class EmdatDataValidator(BaseModelWithExtra):
             raise ValueError("Total deaths cannot be negative.")
         return value
 
-    @classmethod
-    def validate_event(cls, data: dict) -> bool:
-        """Validate the overall data item"""
-        try:
-            _ = cls(**data)  # This will trigger the validators
-        except Exception as e:
-            logger.error(f"Validation failed: {e}")
-            return False
-        # If all field validators return True, we consider it valid
-        return True
-
-    @field_validator(
-        "*",
-        mode="before"
-    )
-    def replace_nan_with_none(cls, value):
+    @field_validator("*", mode="before")
+    def replace_nan_with_none(cls, value: Any, info: ValidationInfo):
+        # Ignore the check for admin_units as it will be an ambiguous check (being a list of dicts)
+        if info.field_name in {"admin_units"}:
+            return value
         if pd.isna(value):
             return None
         return value
