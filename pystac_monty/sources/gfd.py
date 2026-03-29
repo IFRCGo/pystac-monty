5adfcda409168a9987542316c995beb96cbf603d
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2026-03-29 13:33:11 +0545

    GFD: add link relations of type 'related' for related items

--- pystac_monty/sources/gfd.py
+++ pystac_monty/sources/gfd.py
@@ -99,9 +99,16 @@ class GFDTransformer(MontyDataTransformer[GFDDataSource]):
             try:
                 data = GFDSourceValidator(**row)
                 if event_item := self.make_source_event_item(data):
+                    hazard_item = self.make_hazard_event_item(event_item, data)
+                    impact_items = self.make_impact_items(event_item, data)
+
+                    all_items = [event_item, hazard_item] + impact_items
+                    self.set_item_hrefs(items=all_items, eoapi_url=self.data_source.eoapi_url)
+                    self.add_related_links(event_item=event_item, hazard_items=[hazard_item], impact_items=impact_items)
+
                     yield event_item
-                    yield self.make_hazard_event_item(event_item, data)
-                    yield from self.make_impact_items(event_item, data)
+                    yield hazard_item
+                    yield from impact_items
                 else:
                     self.transform_summary.increment_failed_rows()
             except Exception:
